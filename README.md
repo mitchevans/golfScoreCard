a74da87032ad12e574baabe558fbbddf36895cdc
Mitch <mrmtevans@gmail.com>
2019-10-30 06:06:21 -0700

    implemented local storage

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # golfScoreCard
 Deployed Site:  https://mitchevans.github.io/golfScoreCard
 
-A golf scorecard created with Javascript, HTML, CSS and Materialize. Log your score as you play a round of golf. The app will create your boxscore and keep track of your score over or under par. Click on the "Add Score" button after each hole you play. Defaule score is 3 (the app is currently set for an all par 3 course) but you can change your score by clicking on the 3 and inputing another score. Future versions will allow the user to change par for each hole to match wichever course you are playing. Log as many holes as you like.
+A golf scorecard created with Javascript, HTML, CSS and Materialize. 
+
+Log your score as you play a round of golf. This app will create your boxscore and track your score over or under par. Local storage is utilized to persist data throughout your round. 
+
+Click on the "Add Score" button after each hole you play. The default score is 3 (the app is currently set for an all par 3 course) but you can change your score by clicking on the 3 and inputing another score. Future versions will allow the user to change par for each hole to match whichever course the user is playing.
+
+Click on the back arrow button if you need to correct the score on the previous hole you logged. Log as many holes as you can play. 
--- app.js
+++ app.js
@@ -10,12 +10,15 @@ let par = 0;
 let total = 0;
 const date = new Date();
 
-//Set Date
+// Set Date
 document.querySelector('.date').textContent = date.get
 // Load all event listeners
 loadEventListeners();
 
 function loadEventListeners() {
+  // DOM Load Event
+  document.addEventListener('DOMContentLoaded', getScores);
+
   // Add a New Score
   form.addEventListener('submit', addScore);
 
@@ -24,6 +27,75 @@ function loadEventListeners() {
 
   // Clear Card 
   clearBtn.addEventListener('click', clearScores);
+}
+
+// Get Scores from LS
+function getScores() {
+  let scoreArray;
+  if(localStorage.getItem('scoreArray') === null){
+    scoreArray = [];
+  } else {
+    scoreArray = JSON.parse(localStorage.getItem('scoreArray'));
+  }
+
+  scoreArray.forEach(function(score){
+    // Create new div Element
+    const div = document.createElement('div');
+
+    // Add a text node
+    div.appendChild(document.createTextNode(score));
+
+    // Add a class name
+    switch(parseInt(score)) {
+      case 1:
+        div.className = 'ace col s1 square'
+        break;
+      case 2:
+        div.className = 'birdie col s1 square'
+        break;
+        case 3:
+        div.className = 'par col s1 square'
+        break;
+        case 4:
+        div.className = 'bog col s1 square'
+        break;
+        case 5:
+        div.className = 'doublebog col s1 square'
+        break;
+      default:
+    }
+
+    // Append new score div to scorecard
+    scores.appendChild(div);
+  })
+
+  // Set holeNum variable and text from LS
+  if(localStorage.getItem('scoreArray') === null){
+    holeNum.textContent = 1;
+  } else {
+    holeNum.textContent = scoreArray.length + 1;
+  }
+
+  // Set Par Variable from LS
+  par = scoreArray.length * 3;
+  
+  // Set Total Variable from LS
+  for (let i = 0; i < scoreArray.length; i++){
+    total += parseInt(scoreArray[i])
+  }
+  
+  // Set plusMinus
+  plusMinus.textContent = total - par;
+  
+  if (plusMinus.textContent == 0){
+    plusMinus.textContent = 'even'
+  } else if (plusMinus.textContent >= 0 ) {
+    plusMinus.textContent = '+' + plusMinus.textContent
+  } 
+  
+  if(total === 0){
+    plusMinus.textContent = '+-'
+  }
 }
 
 // Add a New Score
@@ -63,13 +135,13 @@ function addScore(e) {
   // Increment Hole Number
   holeNum.textContent++;
 
-  // Increment Par
+  // Add Score to total
   total = total + parseInt(scoreInput.value);
 
-  // Add Score to Total
+  // Increment Par
   par = par + 3
 
-  //Update Plus Minus
+  // Update Plus Minus
   plusMinus.textContent = total - par;
 
   if (plusMinus.textContent == 0){
@@ -78,13 +150,30 @@ function addScore(e) {
     plusMinus.textContent = '+' + plusMinus.textContent
   }
 
-  //Clear input field
+  // Store in LS
+  storeScoreInLocalStorage(scoreInput.value);
+
+  // Clear input field
   scoreInput.value = '';
   
   e.preventDefault();
 }
 
-//Back Button Event
+// Store Score in LS
+function storeScoreInLocalStorage(score){
+  let scoreArray;
+  if(localStorage.getItem('scoreArray') === null){
+    scoreArray = [];
+  } else {
+    scoreArray = JSON.parse(localStorage.getItem('scoreArray'));
+  }
+
+  scoreArray.push(score)
+
+  localStorage.setItem('scoreArray', JSON.stringify(scoreArray));
+}
+
+// Back Button Event
 function backOne() {
 
   // Subtract 3 from par variable
@@ -120,18 +209,45 @@ function backOne() {
   // Remove the last Score div 
   if(scores.firstChild){
     scores.removeChild(scores.lastChild);
+
+    // Remove last score from LS
+    removeScoreFromLocalStorage();
+
   }
 }
 
-//Clear Score Button
+// Remove last score from LS
+function removeScoreFromLocalStorage() {
+  if (localStorage.getItem('scoreArray') === null){
+    scoreArray = [];
+  } else {
+    scoreArray = JSON.parse(localStorage.getItem('scoreArray'));
+  }
+  
+  if(scoreArray.length > 0){
+    scoreArray.splice(scoreArray.length - 1, 1);
+  };
+
+  localStorage.setItem('scoreArray', JSON.stringify(scoreArray));
+}
+
+// Clear Score Button
 function clearScores() {
   while(scores.firstChild){
     scores.removeChild(scores.firstChild);
   }
 
-  //Reset par, total, plusMinus, holeNum variables
+  // Reset par, total, plusMinus, holeNum variables
   par = 0;
   total = 0;
   plusMinus.textContent = '+-'
   holeNum.textContent = 1
+
+  // Clear Local Storage
+  clearScoresFromLocalStorage();
+}
+
+// Clear Local Storage
+function clearScoresFromLocalStorage(){
+  localStorage.clear();
 }
